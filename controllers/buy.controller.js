6aaf013933c4c99466f3852d1ec10abf33cbad6f
hansderly <r.hans4486@gmail.com>
2022-05-05 10:57:42 -0400

    Add contraint to data type

--- controllers/buy.controller.js
+++ controllers/buy.controller.js
@@ -1,7 +1,12 @@
 const buy_gas = async (req, res) => {
+	if (!req.is('application/json'))
+		return res
+			.status(400)
+			.json({ error: 'Bad data format, ONLY SEND JSON' });
+
 	const data = req.body;
 	console.log(data);
-	console.log(req);
+	console.log();
 
 	res.json(data);
 };
